c576d79c6123b0e9f0c2d421c5830ec241c4d44e
Cara-Shen <carashen2001@gmail.com>
2024-09-17 15:24:30 -0400

    replace key with hardcode

--- chatcart/chatcart/settings.py
+++ chatcart/chatcart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tzz6c*tztr%!zbtm6dk^y640p=xn7#go0b8bq8@*xg6e4+54#h'
-
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
